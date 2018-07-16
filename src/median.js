5307c55a6d5d3e3598866b897d4e0ec6e667e8b3
atlassubbed <atlassubbed@gmail.com>
2018-07-15 22:35:23 -0400

    implements median

--- src/median.js
+++ src/median.js
@@ -1,7 +1,10 @@
 module.exports = (arr, isSorted) => {
-  const n = arr.length;
+  const n = arr.length
+  // short circuit all edge cases
   if (!n) return;
   if (n === 1) return arr[0];
   if (n === 2) return (arr[0]+arr[1])/2;
-  
+  const h = n/2;
+  if (!isSorted) arr.sort((a,b) => a-b);
+  return n%2 ? arr[Math.floor(h)] : (arr[h-1]+arr[h])/2;
 }
